47fb0e2aea79de24abfffe3e6659e610859fd4a7
candale <candale.andrei@gmail.com>
2014-08-07 12:06:03 +0300

    Improved Syntax

--- plugins/procedure_name_in_status_bar/status_bar_function.py
+++ plugins/procedure_name_in_status_bar/status_bar_function.py
@@ -1,12 +1,18 @@
 import sublime_plugin
 
 class StatusBarFunctionCommand(sublime_plugin.TextCommand):
-   def run(self, edit):
-      view = self.view
-      region = view.sel()[0]
-      functionRegs = view.find_by_selector('meta.procedure')
-      for r in reversed(functionRegs):
-         if r.a < region.a:
-            view.set_status('function', view.substr(r))
-            return
-      view.erase_status('function')
+	def run(self, edit):
+		view = self.view
+		region = view.sel()[0]
+		functionRegs = view.find_by_selector('entity.name.function.Progress4gl')
+		for r in reversed(functionRegs):
+			if r.a < region.a:
+				txt = view.substr(r)
+				name = txt.split(" ")[1]
+				if ":" in name:
+					name = name.replace(":", "")
+					
+				final_txt = "Procedure %s" % name
+				view.set_status('procedure', final_txt)
+				return
+		view.erase_status('procedure')
